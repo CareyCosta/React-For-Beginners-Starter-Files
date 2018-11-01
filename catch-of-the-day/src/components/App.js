fdbbf0c8c4df92e5df9c5792d71dee2efa86e177
Carey Costa <careycosta@Careys-MacBook-Pro.local>
2018-10-31 22:18:40 -0400

    order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -22,15 +22,14 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
-  addToOrder = (key) => {
+  addToOrder = key => {
     //   take a copy of state
-    const order = {...this.state.order};
+    const order = { ...this.state.order };
     // either add to the orde OR update the number in our order
     order[key] = order[key] + 1 || 1;
     // call setState to update our state object
     this.setState({ order });
-
-  }
+  };
 
   render() {
     return (
@@ -39,11 +38,16 @@ class App extends React.Component {
           <Header tagline="fresh seafood market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
